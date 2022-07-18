ef2a44aa3359f31eeda052920da522df5629bced
LeonardoTheMutant <70893504+LeonardoTheMutant@users.noreply.github.com>
2022-07-18 17:13:24 +0200

    Added "vs. computer"

--- ttt.cpp
+++ ttt.cpp
@@ -7,10 +7,13 @@
 
 
 #include <iostream>
+#include <time.h>
+#include <stdlib.h>
 using namespace std;
 
 //text
-const char welcomeText[]="Welcome to Tic-Tac-Toe!\nThis game requieres two players to play\nHold CTRL+C anytime to leave\n\nNumbers of cells:\n\n1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9";
+const char welcomeText[]="Welcome to Tic-Tac-Toe!\nHold CTRL+C anytime to leave\n\nNumbers of cells:\n\n1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9";
+const char selectPlayerText[]="Select type of new game:";
 const char playerText[]="Player ";
 const char cellPrompt[]=" which cell number to make turn?";
 const char CMDpause[]="PAUSE";
@@ -20,6 +23,9 @@ const char NoNumberText[]="Type only 1-9 numbers!";
 const char NoFreeCellText[]="Type only numbers of free cells!";
 const char winText[]=" win!";
 const char tieText[]="Tie!";
+const char computerText[]="Computer";
+const char vsText[]=" vs. ";
+const char humanText[]="Human";
 char board[9]={32,32,32, 32,32,32, 32,32,32}; //the clear board (by spaces)
 
 //symbols
@@ -42,7 +48,6 @@ const char cross=197;
 
 bool turn=1; //0 - O, 1 - X
 unsigned short cell=1;
-bool play=1; //controls WHILE in main()
 unsigned short filled=0;
 
 int parseInt(char symbol){
@@ -61,6 +66,38 @@ void RESET(char theboard[9]){ //reset the game
 	cell=0;
 	turn=1;
 }
+
+void game(short gametype);
+
+void menu(){ //main menu
+	system(CMDsetresolution);
+	cout << welcomeText << endl;
+	cout << endl;
+	cout << selectPlayerText << endl;
+	cout << 1 << space << humanText << vsText << computerText << endl;
+	cout << 2 << space << computerText << vsText << humanText << endl;
+	cout << 3 << space << computerText << vsText << computerText << endl;
+	cout << 4 << space << humanText << vsText << humanText << endl;
+	int menuSelect=0;
+	cin >> menuSelect;
+	switch(menuSelect){ //game select
+		case 1:
+		    game(1); //human vs com
+			break;
+		case 2:
+		    game(2); //com vs human
+			break;
+		case 3:
+		    game(3); //com vs com, perfect coin flip simulator
+			break;
+		case 4:
+		    game(4); // human vs human
+			break;
+		default:
+		    menu();
+			break;
+	}
+}
 void drawboard(const char theboard[9]){ //draw the board
 	system(CMDclear);
 	cout << tlcorner << bhborder << tcross << bhborder << tcross << bhborder << trcorner << endl;              //top border
@@ -72,7 +109,7 @@ void drawboard(const char theboard[9]){ //draw the board
 	cout << blcorner << bhborder << bcross << bhborder << bcross << bhborder << brcorner << endl;              //bottom border
 	cout << endl;
 }
-void prompt(bool player){ //ask for cell to fill from PLAYER
+void prompt(bool player){ //ask for cell to fill from HUMAN
 	cell=0;
 	if(player){ cout << playerText << X << cellPrompt << endl; }
 	else{ cout << playerText << O << cellPrompt << endl; }
@@ -88,7 +125,16 @@ void prompt(bool player){ //ask for cell to fill from PLAYER
 		prompt(player);
 	}
 }
+void prompt2(bool player){ //ask for cell to fill from COMPUTER
+    do{
+		cell=rand()%9; //get random cell
+	}while(board[cell]!=32);
+	if(player){board[cell]=X;}
+	else{board[cell]=O;}
+	turn=!turn;
+}
 short checkBoard(char theboard[9]){ //check if board is filled or someone won
+    drawboard(theboard);
     filled=0;
     for(int cellnum=0; cellnum<9; cellnum++){ //count how many filled cells
 		if (theboard[cellnum]!=space) filled++;
@@ -106,37 +152,44 @@ short checkBoard(char theboard[9]){ //check if board is filled or someone won
 		system(CMDpause);
 		RESET(theboard);
 		return 1;
-	} else if (theboard[3]==O && theboard[4]==O && theboard[5]==O){
+	}
+	if (theboard[3]==O && theboard[4]==O && theboard[5]==O){
 		cout << playerText << O << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 1;
-	} else if (theboard[6]==O && theboard[7]==O && theboard[8]==O){
+	}
+	if (theboard[6]==O && theboard[7]==O && theboard[8]==O){
 		cout << playerText << O << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 1;
-	} else if (theboard[0]==O && theboard[3]==O && theboard[6]==O){
-		cout << playerText << O << winText << endl << endl;;
-		system(CMDpause);
-		RESET(theboard);
-		return 2;
-	} else if (theboard[1]==O && theboard[4]==O && theboard[7]==O){
-		cout << playerText << O << winText << endl << endl;;
-		system(CMDpause);
-		RESET(theboard);
-		return 2;
-	} else if (theboard[2]==O && theboard[5]==O && theboard[8]==O){
-		cout << playerText << O << winText << endl << endl;;
-		system(CMDpause);
-		RESET(theboard);
-		return 2;
-	} else if (theboard[0]==O && theboard[4]==O && theboard[8]==O){
+	}
+	if (theboard[0]==O && theboard[3]==O && theboard[6]==O){
 		cout << playerText << O << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 1;
-	} else if (theboard[2]==O && theboard[4]==O && theboard[6]==O){
+	}
+	if (theboard[1]==O && theboard[4]==O && theboard[7]==O){
+		cout << playerText << O << winText << endl << endl;;
+		system(CMDpause);
+		RESET(theboard);
+		return 1;
+	}
+	if (theboard[2]==O && theboard[5]==O && theboard[8]==O){
+		cout << playerText << O << winText << endl << endl;;
+		system(CMDpause);
+		RESET(theboard);
+		return 1;
+	}
+	if (theboard[0]==O && theboard[4]==O && theboard[8]==O){
+		cout << playerText << O << winText << endl << endl;;
+		system(CMDpause);
+		RESET(theboard);
+		return 1;
+	}
+	if (theboard[2]==O && theboard[4]==O && theboard[6]==O){
 		cout << playerText << O << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
@@ -144,41 +197,48 @@ short checkBoard(char theboard[9]){ //check if board is filled or someone won
 	}
 	//check for X
 	if (theboard[0]==X && theboard[1]==X && theboard[2]==X){
-		cout << playerText << O << winText << endl << endl;;
-		system(CMDpause);
-		RESET(theboard);
-		return 2;
-	} else if (theboard[3]==X && theboard[4]==X && theboard[5]==X){
-		cout << playerText << O << winText << endl << endl;;
-		system(CMDpause);
-		RESET(theboard);
-		return 2;
-	} else if (theboard[6]==X && theboard[7]==X && theboard[8]==X){
 		cout << playerText << X << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 2;
-	} else if (theboard[0]==X && theboard[3]==X && theboard[6]==X){
+	}
+	if (theboard[3]==X && theboard[4]==X && theboard[5]==X){
 		cout << playerText << X << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 2;
-	} else if (theboard[1]==X && theboard[4]==X && theboard[7]==X){
+	}
+	if (theboard[6]==X && theboard[7]==X && theboard[8]==X){
 		cout << playerText << X << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 2;
-	} else if (theboard[2]==X && theboard[5]==X && theboard[8]==X){
+	}
+	if (theboard[0]==X && theboard[3]==X && theboard[6]==X){
 		cout << playerText << X << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 2;
-	} else if (theboard[0]==X && theboard[4]==X && theboard[8]==X){
+	}
+	if (theboard[1]==X && theboard[4]==X && theboard[7]==X){
 		cout << playerText << X << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
 		return 2;
-	} else if (theboard[2]==X && theboard[4]==X && theboard[6]==X){
+	}
+	if (theboard[2]==X && theboard[5]==X && theboard[8]==X){
+		cout << playerText << X << winText << endl << endl;;
+		system(CMDpause);
+		RESET(theboard);
+		return 2;
+	}
+	if (theboard[0]==X && theboard[4]==X && theboard[8]==X){
+		cout << playerText << X << winText << endl << endl;;
+		system(CMDpause);
+		RESET(theboard);
+		return 2;
+	}
+	if (theboard[2]==X && theboard[4]==X && theboard[6]==X){
 		cout << playerText << X << winText << endl << endl;;
 		system(CMDpause);
 		RESET(theboard);
@@ -186,16 +246,39 @@ short checkBoard(char theboard[9]){ //check if board is filled or someone won
 	}
 	return 0;
 }
-
-//driver code
-int main(){
-	system(CMDsetresolution);
-	cout << welcomeText << endl;
-	system(CMDpause);
-	while(play){
+void game(short gametype){
+	while(1){
 		drawboard(board);
-		prompt(turn);
+		switch(gametype){
+			case 1:
+			    prompt(turn);
+				checkBoard(board);
+				drawboard(board);
+				prompt2(turn);
+			    break;
+			case 2:
+			    prompt2(turn);
+				checkBoard(board);
+				drawboard(board);
+				prompt(turn);
+			case 3:
+			    prompt2(turn);
+				break;
+			case 4:
+			    prompt(turn);
+				break;
+			default: //if unknown gametype is given, just exit
+			    exit(-1);
+				break;
+		}
 		checkBoard(board);
 	}
+	
+}
+
+//driver code, empty though, I guess you should go check menu();
+int main(){
+	srand(time(0)); //init the random seed
+	menu();
 	return 0;
 }
